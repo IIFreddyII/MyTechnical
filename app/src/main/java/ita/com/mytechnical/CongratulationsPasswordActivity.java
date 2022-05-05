512f647809b9138292d8b28674996f56cc07ab28
IIFreddyII <alfredolunacastro07@gmail.com>
2022-05-05 15:23:09 -0500

    Se agregaron los Layout(ChangePassword, congratulationsPassword, PasswordChange, Profile,)

---
+++ app/src/main/java/ita/com/mytechnical/CongratulationsPasswordActivity.java
@@ -0,0 +1,14 @@
+package ita.com.mytechnical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CongratulationsPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congratulations_password);
+    }
+}
